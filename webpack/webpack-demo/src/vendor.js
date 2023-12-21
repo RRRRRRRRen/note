e530e994efa4c0bae9758603ee6164f203cfacc1
rrrrrrrren <dittorenard@outlook.co>
2023-12-21 23:00:36 +0800

    调整目录结构

--- webpack/webpack-demo/src/vendor.js
+++
@@ -1,9 +0,0 @@
-import "./assets/index.css"
-import "./assets/index.less"
-
-const func = (name) => {
-  return name + " OK";
-};
-
-const firstName = "Tik";
-console.log("firstName :>> ", func(firstName));
